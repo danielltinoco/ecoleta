7be05431d12a24048bd7abdc2d76409b40a50b27
danielltinoco <danielltinoco@gmail.com>
2020-06-07 13:40:37 -0300

    adding final project

---
+++ public/scripts/intro.js
@@ -0,0 +1,73 @@
+// comments
+// document.write("Vasco")
+
+// variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// "isso é uma string"
+// 'isso tbm é uma strig'
+// ´isso é string tbm´
+
+// number
+// const n1 = "1"
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidades
+//const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: true,
+//  correr(){     
+//    document.write("executar uma grande logica de correr")
+//  }
+//}
+
+//pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// // registra função
+// function sayMyName(name) {
+  
+//   document.write(name)
+// }
+
+// //executar
+// sayMyName("Douglas")
+// sayMyName("Daniel")
+// sayMyName("Lucas")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   // caminho 1
+//   document.write("Reprovado")
+// } else {
+//   // caminho 2]
+//   document.write("Aprovado")
+// }
+
+// loop repetições
+
+// for (i=0; i< 10; i++ //i = i + 1 
+//                       ) {
+//   document.write(`<p>${i}</p>`)
+// }
